cbbec757856ea8f51786fe65616435f6b3ca528f
KUNAL JOSHI <globalinformationtracker2002@gmail.com>
2024-07-23 22:31:09 +0800

    Create sonar.sh

---
+++ sonar.sh
@@ -0,0 +1,12 @@
+t/
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
+unzip sonarqube-8.9.6.50800.zip
+amazon-linux-extras install java-openjdk11 -y
+useradd sonar
+chown sonar:sonar sonarqube-8.9.6.50800 -R
+chmod 777 sonarqube-8.9.6.50800 -R
+su - sonar
+
+#run this on server manually
+#sh /opt/sonarqube-8.9.6.50800/bin/linux/sonar.sh start
+#echo "user=admin & password=admin"
